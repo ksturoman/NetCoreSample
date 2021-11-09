2c8a3f7f5dbb87dfc101b3639b24122191da1dea
ksturoman <32057211+ksturoman@users.noreply.github.com>
2021-11-09 14:29:21 +0300

    Anonymous method for testing added

---
+++ NetCoreSample/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NetCoreSample.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok($"Hello world! The time is {DateTime.Now}");
+    }
+}
